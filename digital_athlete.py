c7cefbaeb747fae3e176d4147a07351a869f5271
ciklamini <115336048+ciklamini@users.noreply.github.com>
2024-01-27 10:33:53 +0100

    Update digital_athlete.py

--- digital_athlete.py
+++ digital_athlete.py
@@ -1,25 +1,3 @@
 
 import pandas as pd
-import dash
-from dash import Dash, dcc, html, Input, Output
-import dash_vtk
-from dash import html
-import numpy as np
-import vtk
-from dash_vtk.utils import to_mesh_state
-app = dash.Dash(__name__)
-server = app.server
-app.layout = html.Div(
-    style={"height": "calc(100vh - 16px)"},
-    children=[
-                dcc.Dropdown(['DA' ], 
-                              'DA', id='demo-dropdown'),
-                html.Div(id='dd-output-container', style={"height": "100%", "width": "100%", }),    
-                ]  )
-@app.callback(
-    Output('dd-output-container', 'children'),
-    Input('demo-dropdown', 'value')
-)
-
-if __name__ == "__main__":    app.run_server(debug=True)
-
+ print('yeah')
