6f5e1cb30c3c9fe7104a03413d11c504cf1e3533
ciklamini <115336048+ciklamini@users.noreply.github.com>
2024-08-09 13:55:03 +0200

    Update digital_athlete.py 34

--- digital_athlete.py
+++ digital_athlete.py
@@ -1,82 +1,117 @@
-import dash  
-from dash import html, dcc, Input, Output, State  
-import dash_bootstrap_components as dbc  
-import base64  
-import io  
-from PIL import Image  
-import dash_core_components as dcc  
-import dash_html_components as html  
-  
-# Initialize the Dash app  
-app = dash.Dash(__name__)  
-  
-app.layout = html.Div([  
-    # Header  
-    html.H1("Image Upload and Visualization App"),  
-      
-    # Upload component  
-    dcc.Upload(  
-        id='upload-image',  
-        children=html.Div([  
-            'Drag and Drop or ',  
-            html.A('Select Files')  
-        ]),  
-        style={  
-            'width': '100%',  
-            'height': '60px',  
-            'lineHeight': '60px',  
-            'borderWidth': '1px',  
-            'borderStyle': 'dashed',  
-            'borderRadius': '5px',  
-            'textAlign': 'center',  
-            'margin': '10px'  
-        },  
-        # Allow multiple files to be uploaded  
-        multiple=False  
-    ),  
-  
-    # Layout with two columns  
-    dbc.Row([  
-        # Left column with image visualization  
-        dbc.Col([  
-            html.Div(id='output-image-upload')  
-        ], width=6),  
-  
-        # Right column with string description in markdown  
-        dbc.Col([  
-            dcc.Markdown(id='image-description')  
-        ], width=6)  
-    ])  
-])  
-  
-def parse_contents(contents):  
-    content_type, content_string = contents.split(',')  
-  
-    decoded = base64.b64decode(content_string)  
-    image = Image.open(io.BytesIO(decoded))  
-    return image  
-  
-@app.callback(  
-    [Output('output-image-upload', 'children'),  
-     Output('image-description', 'children')],  
-    [Input('upload-image', 'contents')],  
-)  
-def update_output(contents):  
-    if contents is not None:  
-        image = parse_contents(contents)  
-        image_visual = html.Img(src=contents, style={'width': '100%'})  
-  
-        # Here you can define your string description  
-        string_description = """  
-        ### Image Description  
-        - **Format:** {}  
-        - **Size:** {}x{}  
-        """.format(image.format, image.width, image.height)  
-  
-        return image_visual, string_description  
-    else:  
-        return None, None  
-  
-# Run the app  
-if __name__ == '__main__':  
-    app.run_server(debug=True)  
+# -*- coding: utf-8 -*-
+"""
+goal to drop dabra 3D point tracks 
+show at datable 
+"""
+
+from dash import Dash, dcc, html, dash_table, Input, Output, State, callback
+
+import base64
+import datetime
+import io
+import plotly.express as px 
+import pandas as pd
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=True
+    ),
+    html.Div(id='output-data-upload'),
+])
+
+def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+        elif 'emt' in filename:
+            # Assume that the user uploaded emt file 
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')),  skiprows=9, delimiter=r"\s+")
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+
+    return html.Div([
+        # markdown pro describe 
+        dcc.Markdown('''
+        ## LaTeX in a Markdown component:
+    
+        This example uses the block delimiter:
+        $$
+        \\frac{1}{(\\sqrt{\\phi \\sqrt{5}}-\\phi) e^{\\frac25 \\pi}} =
+        1+\\frac{e^{-2\\pi}} {1+\\frac{e^{-4\\pi}} {1+\\frac{e^{-6\\pi}}
+        {1+\\frac{e^{-8\\pi}} {1+\\ldots} } } }
+        $$
+    
+        This example uses the inline delimiter:
+        $E^2=m^2c^4+p^2c^2$
+    
+        ## LaTeX in a Graph component:
+    
+        ''', mathjax=True),
+        # ukazat vse pomoci car 
+        # fig = px.line(df, x='ID', y=df.columns),
+        
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(date)),
+
+        dash_table.DataTable(
+            data = df.to_dict('records'),
+            columns = [{'name': i, 'id': i} for i in df.columns],
+            page_size=5,
+            
+        ),
+
+        html.Hr(),  # horizontal line
+
+        # For debugging, display the raw contents provided by the web browser
+        html.Div('Raw Content'),
+        html.Pre(contents[0:200] + '...', style={
+            'whiteSpace': 'pre-wrap',
+            'wordBreak': 'break-all'
+        })
+    ])
+
+@callback(Output('output-data-upload', 'children'),
+              Input('upload-data', 'contents'),
+              State('upload-data', 'filename'),
+              State('upload-data', 'last_modified'))
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
+
+if __name__ == '__main__':
+    app.run(debug=True)
